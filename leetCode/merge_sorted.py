51868aea37918526f023140e1a81f9c704334058
ConnerM Windows <manwillconner3@gmail.com>
2025-03-24 18:16:35 -0600

    commiting from Windows PC

--- leetCode/merge_sorted.py
+++ leetCode/merge_sorted.py
@@ -22,8 +22,7 @@ def merge(nums1: list[int], m: int, nums2: list[int], n: int) -> None:
             nums1[ridx] = nums2[nidx]
             nidx -= 1
 
-        ridx -= 1
-
+        ridx -= 
     print(nums1)
 
 merge(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3)

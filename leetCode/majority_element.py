2e7ac24d2449f579155dc9e2f766f9468f6e2f42
Conner Manwill <140928079+ConnerM2@users.noreply.github.com>
2025-03-25 21:37:20 -0600

    started leetcode majority_element

---
+++ leetCode/majority_element.py
@@ -0,0 +1,17 @@
+# Example 1:
+
+# Input: nums = [3,2,3]
+# Output: 3
+# Example 2:
+
+# Input: nums = [2,2,1,1,1,2,2]
+# Output: 2
+
+def majority_element(nums: list[int]) -> int:
+    my_dict = {}
+
+    for num in nums:
+        
+
+
+print(majority_element([2,2,1,1,1,2,2]))

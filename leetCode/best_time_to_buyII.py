e8c8b70a222070bc9d80f39a62958ffa4d76cf39
ConnerM Windows <manwillconner3@gmail.com>
2025-03-29 19:06:24 -0600

    Best time to sell 2

--- leetCode/best_time_to_buyII.py
+++ leetCode/best_time_to_buyII.py
@@ -14,4 +14,12 @@
 # Total profit is 4.
 
 def sell_stonk(prices: list[int]) -> int:
-    pass
+    total_profit = 0
+
+    for i in range(1, len(prices)):
+        if prices[i] > prices[i-1]:
+            total_profit += prices[i] - prices[i-1]
+
+    return total_profit
+
+print(sell_stonk([7,1,5,3,6,4]))

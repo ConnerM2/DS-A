41c7557f41bba42dd41b51939483d7a16ef43449
ConnerM Windows <manwillconner3@gmail.com>
2025-05-22 15:44:51 -0600

    added onto linked list

--- udemy_DSA/data_structures/linked_lists/linked_lists.py
+++ udemy_DSA/data_structures/linked_lists/linked_lists.py
@@ -1,8 +1,10 @@
-# Linked lists are a list like data structure where each node points to the next node
-# There are no indexes, just a head, nodes, and tails
-# Instead of being right next to each other in memory, like a list, they are spread out
-# Nodes are essentially a dictionary
-#      {"value": 10, "next": None}
+"""
+Linked lists are a list like data structure where each node points to the next node
+There are no indexes, just a head, nodes, and tails
+Instead of being right next to each other in memory, like a list, they are spread out
+Nodes are essentially a dictionary
+     {"value": 10, "next": None}
+"""
 
 
 # Creating a linked list
@@ -32,14 +34,26 @@ class LinkedList:
         return True
     
     def pop(self):
+        if self.length == 0: # Returns None is there is nothing in your LL
+            return None
         temp = self.head
         pre = self.head
+         # Uses temp and pre var to loop through LL
+         # while temp.next != none, we increment forward
+         # Once it = None, tail is set to pre and tail.next is set to None
         while temp.next is not None:
             pre = temp
             temp = temp.next
         self.tail = pre
         self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
         return temp
+    
+    def prepend(self, value):
+        
 
     def print_list(self):
         temp = self.head
